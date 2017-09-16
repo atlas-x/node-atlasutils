63aff7e61fe8e876fafc2526c81bfdb873591b9c
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2017-09-15 21:51:21 -0400

    fixing exports for js

--- dist/middleware.d.ts
+++
@@ -1,8 +0,0 @@
-export interface MiddlewareConfig {
-    log?: string[];
-    logger?: any;
-    logUrl?: boolean;
-    getUser?: (req: any) => string | void;
-}
-export default function middleware(req: any, res: any, next: any): void;
-export declare function configure(config?: MiddlewareConfig): void;
--- logger.js
+++ logger.js
@@ -1,2 +1,18 @@
 // used for `require('atlasutils/middleware')` - source in ./src
-module.exports = require('./dist/lib/logger');
+let logger = require('./dist/lib/logger');
+
+let Logger = logger.default;
+
+function defaultExport(filename) {
+  return new Logger(filename);
+}
+defaultExport.log = Logger.log;
+defaultExport.silly = Logger.silly;
+defaultExport.debug = Logger.debug;
+defaultExport.info = Logger.info;
+defaultExport.warn = Logger.warn;
+defaultExport.error = Logger.error;
+
+defaultExport.configure = logger.configure;
+
+module.exports = defaultExport;
