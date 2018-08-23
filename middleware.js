a44875b64399179b726567f12a237ea4794e7737
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2018-08-23 16:11:08 -0400

    bump

--- middleware.js
+++ middleware.js
@@ -3,5 +3,7 @@ let mw = require('./dist/middleware');
 
 let exportFunc = mw.middleware;
 exportFunc.configure = mw.configure;
+exportFunc.logRequests = mw.logRequests;
+
 
 module.exports = exportFunc;
